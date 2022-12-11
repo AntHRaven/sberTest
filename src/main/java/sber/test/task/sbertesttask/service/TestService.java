58bbb9449d3643a5deaced732927ef75718aa3e0
Nikita Veko <n.veko@iac.spb.ru>
2022-12-11 20:33:57 +0300

    secured with aspect

--- src/main/java/sber/test/task/sbertesttask/service/TestService.java
+++ src/main/java/sber/test/task/sbertesttask/service/TestService.java
@@ -8,17 +8,20 @@ import sber.test.task.sbertesttask.annotation.MySecuredAnnotation;
 @Service
 public class TestService {
 
+//  @Secured("ROLE_USER")
   @MySecuredAnnotation("ROLE_USER")
   public String test1() {
     return "test1";
   }
 
+//  @Secured("ROLE_ADMIN")
   @MySecuredAnnotation("ROLE_ADMIN")
   public String test2() {
     return "test2";
   }
 
 
+//  @Secured({ "ROLE_USER", "ROLE_ADMIN" })
   @MySecuredAnnotation({ "ROLE_USER", "ROLE_ADMIN" })
   public String test3() {
     return "test3";
